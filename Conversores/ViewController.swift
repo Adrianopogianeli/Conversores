d5ba8ee10faa659effefa59e4c9b6db51efb500c
ARPL <adriano.pogianeli@gmail.com>
2018-04-16 11:53:41 -0300

    first commit

---
+++ Conversores/ViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  Conversores
+//
+//  Created by admin on 4/16/18.
+//  Copyright © 2018 adriano. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    @IBOutlet weak var tfValue: UIView!
+    @IBOutlet weak var btUnit1: UIButton!
+    @IBOutlet weak var btUnit2: UIButton!
+    @IBOutlet weak var lbResult: UILabel!
+    @IBOutlet weak var lbResultUnit: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    @IBAction func showNext(_ sender: UIButton) {
+    }
+    
+    @IBAction func convert(_ sender: UIButton) {
+    }
+    
+
+
+}
+
